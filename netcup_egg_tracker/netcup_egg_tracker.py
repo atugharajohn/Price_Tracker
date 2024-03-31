2156ea2770de8ef304a00262f544b92c4125882c
atugharajohn <atugharajohn@gmail.com>
2024-03-31 19:59:17 +0200

    add folder year name

--- netcup_egg_tracker/netcup_egg_tracker.py
+++ netcup_egg_tracker/netcup_egg_tracker.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import os
+from datetime import datetime
 
 refs = [
     "/vserver/vserver_images.php",
@@ -19,24 +20,37 @@ refs = [
 ]
 
 def get_price_formatted(price):
-    # Formatieren Sie den Preis, damit er in den Dateinamen eingefügt werden kann
     return price.replace(",", ".").replace("&euro;", "EUR").replace(" ", "")
 
 def main():
     while True:
+        current_year = datetime.now().year  # Aktuelles Jahr abrufen
+        folder_path = f"eggs_{current_year}"  # Ordnername mit aktuellem Jahr erstellen
+        
         for r in refs:
             try:
                 resp = requests.post("https://www.netcup.de/api/eggs", data={"requrl": r})
                 egg = json.loads(resp.text)["eggs"][0]
-                print(egg)
                 price = get_price_formatted(egg["price"])
-                name = f"{price}Euro_{egg['id']}__{egg['title']}.txt"
+                name = f"{price}Euro_{egg['id']}__{egg['title']}.json"
                 name = name.replace("/", "_").replace("|", "_").replace("\\", "_").replace(":", "_").replace("*", "_").replace("?", "_").replace('"', "_").replace("<", "_").replace(">", "_")
-                with open(name, "w") as file:
-                    file.write(f"https://www.netcup.de/bestellen/produkt.php?produkt={egg['product_id']}&hiddenkey={egg['product_key']}&ref=230003\n{json.dumps(egg)}\n{r}")
+                
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
+                
+                egg['original_url'] = f"https://www.netcup.de/bestellen/produkt.php?produkt={egg['product_id']}&ref=230003&hiddenkey={egg['product_key']}"
+                with open(os.path.join(folder_path, name), "w") as file:
+                    json.dump(egg, file, indent=4)
+                
+                print(f"{'-' * 10}")
+                print(f"{egg['title']}")
+                print(f"{price}")
+                print(f"{egg['original_url']}")
+                print(f"{'-' * 10}")
             except Exception as e:
-                print(f"Error: {e}")
-        print(f"\n\n Time Sleep - {10*60}")
+                pass
+        
+        print(f"\n\n Time Sleep - {2*60}")
         time.sleep(2 * 60)
 
 if __name__ == "__main__":
